085e4ce8af2465c4d79ae507a1ffaace81ad94ef
Limerio <44976231+Limerio@users.noreply.github.com>
2020-10-14 22:31:14 +0200

    Panel frontend finished, update after the database

--- routes/panel.js
+++ routes/panel.js
@@ -10,6 +10,7 @@ let bans = [
 ];
 
 router.get("/", isAuth, (req, res) => {
+    console.log(bans)
     res.render("panel", {
         title: "Panel page",
         username: req.user.username,
@@ -29,7 +30,7 @@ router.post("/banip/new", isAuth, (req, res) => {
     if (!ip) return res.status(400).send("Unknown ip");
     if (!timeofban) return res.status(400).send("Unknown time of ban");
 
-    if (!bans.find(ban => ban.ip === ip)) {
+    if (!bans.find(ban => ban && ban.ip === ip)) {
         bans.push({
             ip,
             timeofban,
@@ -42,12 +43,11 @@ router.post("/banip/new", isAuth, (req, res) => {
 
 router.get("/banip/edit/:ip", isAuth, (req, res) => {
     const { ip } = req.params;
-
-    if (bans.find(ban => ban.ip === ip)) {
-        res.render("banip/edit", {
+    if (bans.find(ban => ban && ban.ip === ip)) {
+        return res.render("banip/edit", {
             title: "Edit a ip address",
             username: req.user.username,
-            ban: bans.find(ban => ban.ip === ip),
+            ban: bans.find(ban => ban && ban.ip === ip),
         })
     }
     return res.status(400).send("Unknown ip");
@@ -57,32 +57,35 @@ router.post("/banip/edit/:oldIP", isAuth, (req, res) => {
     const { oldIP } = req.params;
     let { ip, bantime, timeofban } = req.body;
 
-    if (!bans.find(ban => ban.ip === oldIP)) return res.status(400).send("Unknown ip");
+    if (!bans.find(ban => ban && ban.ip === oldIP)) return res.status(400).send("Unknown ip");
 
-    bantime = bans.find(ban => ban.ip === oldIP).bantime
+    const bansIp = bans.find(ban => ban.ip === oldIP)
+    bantime = bansIp.bantime
 
     try {
-        delete bans[bans.findIndex(ban => ban.ip === oldIP)];
-        bans.push({
-            ip,
-            timeofban,
-            bantime
-        });
+        if (bansIp.ip !== ip) {
+            bansIp.ip = ip
+        }
+
+        if (bansIp.timeofban !== timeofban) {
+            bansIp.timeofban = timeofban
+        }
     } catch (e) {
         console.error(e);
         return res.sendStatus(500);
     }
 
-    res.redirect("/panel");
+    return res.redirect("/panel");
 });
 
-router.delete("/banip/delete/:ip", isAuth, (req, res) => { 
+router.post("/banip/delete/:ip", isAuth, (req, res) => { 
     const { ip } = req.params;
 
-    if (!bans.find(ban => ban.ip === ip)) return res.status(400).send("Unknown ip");
+    if (!bans.find(ban => ban && ban.ip === ip)) return res.status(400).send("Unknown ip");
 
-    delete bans[bans.findIndex(ban => ban.ip === ip)];
-    res.location("/panel")
+    delete bans[bans.findIndex(ban => ban && ban.ip === ip)];
+
+    return res.redirect("/panel");
 })
 
 module.exports = router;
